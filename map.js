e0aac22b6c0fc0a4445dde62331b66f5c97f8cfb
arlealexandre <arle.alexandre@protonmail.com>
2021-05-28 18:05:26 +0200

    Ajout de 5 markers

--- map.js
+++ map.js
@@ -9,10 +9,42 @@ var map = new mapboxgl.Map({
 .setMaxZoom(10)
 .setMinZoom(9);
 
-var marker = new mapboxgl.Marker({
+var marker1 = new mapboxgl.Marker({
     color: '#171717',
     draggable: false
 })
 .setLngLat([-75,40])
-.setPopup(new mapboxgl.Popup().setMaxWidth("500px").setHTML("<div id='popup'><div id='Image'><h2>Image</h2></div><div id='description'><h2>Name of the listing</h2><p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><h3>Technical info :</h3><ul><li>Superficie</li><li>Nombre de chambre</li></ul><p><input type='submit' value='RESERVER' src='www.google.com'></p></div></div>"))
+.setPopup(new mapboxgl.Popup().setMaxWidth("300px").setHTML("<div id='popup'><img class='Image' src='img/loft1.jpg'><div id='description'><h2>Name of the listing</h2><p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><h3>Technical info :</h3><ul><li>Superficie</li><li>Nombre de chambre</li></ul><p><input type='submit' value='RESERVER' src='www.google.com'></p></div></div>"))
+.addTo(map);
+
+var marker2 = new mapboxgl.Marker({
+    color: '#171717',
+    draggable: false
+})
+.setLngLat([-75.1,40.1])
+.setPopup(new mapboxgl.Popup().setMaxWidth("300px").setHTML("<div id='popup'><img class='Image' src='img/loft2.jpg'><div id='description'><h2>Name of the listing</h2><p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><h3>Technical info :</h3><ul><li>Superficie</li><li>Nombre de chambre</li></ul><p><input type='submit' value='RESERVER' src='www.google.com'></p></div></div>"))
+.addTo(map);
+
+var marker3 = new mapboxgl.Marker({
+    color: '#171717',
+    draggable: false
+})
+.setLngLat([-74.8,39.9])
+.setPopup(new mapboxgl.Popup().setMaxWidth("300px").setHTML("<div id='popup'><img class='Image' src='img/loft3.jpeg'><div id='description'><h2>Name of the listing</h2><p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><h3>Technical info :</h3><ul><li>Superficie</li><li>Nombre de chambre</li></ul><p><input type='submit' value='RESERVER' src='www.google.com'></p></div></div>"))
+.addTo(map);
+
+var marker4 = new mapboxgl.Marker({
+    color: '#171717',
+    draggable: false
+})
+.setLngLat([-75.2,39.8])
+.setPopup(new mapboxgl.Popup().setMaxWidth("300px").setHTML("<div id='popup'><img class='Image' src='img/loft4.jpeg'><div id='description'><h2>Name of the listing</h2><p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><h3>Technical info :</h3><ul><li>Superficie</li><li>Nombre de chambre</li></ul><p><input type='submit' value='RESERVER' src='www.google.com'></p></div></div>"))
+.addTo(map);
+
+var marker5 = new mapboxgl.Marker({
+    color: '#171717',
+    draggable: false
+})
+.setLngLat([-75.3,39.95])
+.setPopup(new mapboxgl.Popup().setMaxWidth("300px").setHTML("<div id='popup'><img class='Image' src='img/loft5.jpeg'><div id='description'><h2>Name of the listing</h2><p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><h3>Technical info :</h3><ul><li>Superficie</li><li>Nombre de chambre</li></ul><p><input type='submit' value='RESERVER' src='www.google.com'></p></div></div>"))
 .addTo(map);
