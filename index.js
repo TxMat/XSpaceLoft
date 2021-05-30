2e55be1e3197499ec82517f00074ead9625e96e2
arlealexandre <arle.alexandre@protonmail.com>
2021-05-30 18:45:31 +0200

    MaJ du container Filter

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 
+    var count = 0;
+
     $("#filter").click(function() {
         $("#container-filter").fadeToggle();
+        $("#filter").css("border-radius","50px");
+        count += 1;
+        if ((count%2)==0) {
+            $("#filter").css("border-radius","5px 5px 0 0");
+        }
     });
+
 });
